61af385164af8b1c73e9f86c4545eb1968825bc1
meteor199 <meteor199@qq.com>
2023-05-20 15:55:14 +0800

    test: add renderer invoke renderer unit test

--- packages/electron-message-center/__tests__/invoke.spec.ts
+++ packages/electron-message-center/__tests__/invoke.spec.ts
@@ -269,5 +269,80 @@ describe('invoke test', () => {
       const ret = await messageCenterRenderer.invoke(route, 1);
       expect(ret).toBe(undefined);
     });
+
+    describe('渲染进程 invoke 渲染进程', () => {
+      it('假如有多个参数，参数应该都正确接收', async () => {
+        function first(...args: unknown[]) {
+          expect(route).toBe(route);
+          expect(args[0]).to.equal(1);
+          expect(args[1]).to.equal(null);
+          expect(args[2]).to.equal('x');
+          expect(args[3]).toMatchObject({ a: 1 });
+          return 2;
+        }
+        messageCenterRenderer.on(route, first);
+        await sleep(1);
+        const ret = await messageCenterRenderer.invoke(route, 1, null, 'x', { a: 1 }, new Error('test'));
+        expect(ret).toBe(2);
+      });
+
+      it('当返回普通类型时，应收到返回值', async () => {
+        function first(num: number) {
+          return num + 1;
+        }
+        messageCenterRenderer.on(route, first);
+        await sleep(1);
+        const ret = await messageCenterRenderer.invoke(route, 1);
+        expect(ret).toBe(2);
+      });
+
+      it('当返回Promise类型时，应收到返回值', async () => {
+        async function first(num: number) {
+          await sleep(1);
+          return num + 2;
+        }
+        messageCenterRenderer.on(route, first);
+        await sleep(1);
+        const ret = await messageCenterRenderer.invoke(route, 1);
+        expect(ret).toBe(3);
+      });
+
+      it('当抛出异常时，应接收到错误', async () => {
+        function first() {
+          throw new Error('error test');
+        }
+        messageCenterRenderer.on(route, first);
+        await sleep(1);
+        try {
+          await messageCenterRenderer.invoke(route, 1);
+          expect.fail('不应该正常返回');
+        } catch (err) {
+          expect((err as Error).message).toBe('error test');
+        }
+      });
+      it('当是返回异步错误时(Promise.reject)，应接收到错误', async () => {
+        function first() {
+          return Promise.reject(new Error('error promise test'));
+        }
+        messageCenterRenderer.on(route, first);
+        await sleep(1);
+        try {
+          await messageCenterRenderer.invoke(route, 1);
+          expect.fail('不应该正常返回');
+        } catch (err) {
+          expect((err as Error).message).toBe('error promise test');
+        }
+      });
+
+      it('当是监听者没有返回值时，应接收到undefined', async () => {
+        function first() {
+          //
+        }
+        messageCenterRenderer.on(route, first);
+        await sleep(1);
+        const ret = await messageCenterRenderer.invoke(route, 1);
+        expect(ret).toBe(undefined);
+      });
+    });
   });
 });
