7a6ed3872ea4a69b7c256e48b0f0ca6b73c6fc40
meteor199 <meteor199@qq.com>
2023-11-15 20:06:40 +0800

    docs: add contribution guidelines, enhance function comments

--- packages/electron-message-center/src/shared/base.ts
+++ packages/electron-message-center/src/shared/base.ts
@@ -11,8 +11,24 @@ export abstract class MessageCenterBase {
    * @param {...any} args - The arguments to send with the message.
    */
   public abstract broadcast(route: string, ...dataArgs: unknown[]): void;
+  /**
+   * Register a listener for a specific channel.
+   * @param {string} route - The route to send the message to.
+   * @param {Listener} listener - The listener to register.
+   */
   public abstract on(route: string, listener: Listener): void;
+  /**
+   * Unregister a listener for a specific channel.
+   * @param {string} route - The route to remove the listener from.
+   * @param {Listener} [listener] - The listener to unregister. If not provided, all listeners for the specified route will be removed.
+   */
   public abstract off(route: string, listener?: Listener): void;
+
+  /**
+   * Retrieve all registered listeners for a specific channel or all channels.
+   * @param {string} [route] - The route to get listeners for. If not provided, returns all listeners for all routes.
+   * @returns {Promise<ListenerInfo[]>} - A promise that resolves with an array of ListenerInfo objects.
+   */
   public abstract getAllListeners(route?: string): Promise<ListenerInfo[]>;
   /**
    * Send a message to the main process or renderers via `route` and expect a result asynchronously.
